14b27259417988ba82e8eb1e88e8e7da87378e29
Liliana327 <liliuospina@gmial.com>
2020-10-26 09:05:43 -0500

    add the form to login

--- src/components/security/Login.js
+++ src/components/security/Login.js
@@ -1,4 +1,4 @@
-import { Avatar, Container, Typography } from '@material-ui/core';
+import { Avatar, Button, Container, TextField, Typography } from '@material-ui/core';
 import React, { Component } from 'react';
 import LockOutlineIcon from '@material-ui/icons/LockOutlined'
 
@@ -12,6 +12,10 @@ const style={
     avatar: {
         margin: 5,
         backgroundColor: "#ffb300"
+    },
+    form: {
+        width: "100%",
+        marginTop: 8
     }
 }
 
@@ -27,6 +31,31 @@ class Login extends Component {
                     <Typography component="h1" variant="h5">
                         Ingrese Usuario
                     </Typography>
+                    <form style={style.form}>
+                        <TextField
+                            variant="outlined"
+                            label="E-mail"
+                            name="email"
+                            fullWidth
+                            margin="normal"
+                        />
+                        <TextField 
+                            variant="outlined"
+                            label="Password"
+                            type="password"
+                            name="password"
+                            fullWidth
+                            margin="normal"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            Ingresar
+                        </Button>
+                    </form>
                 </div>
             </Container>
         );
